2247f0df4f8c495993d395132df05fe3df541eba
nomadcgrang9 <l30417305@gmail.com>
2025-11-05 13:44:22 +0900

    Update PWA caching strategy
    - Change Supabase caching from NetworkFirst to CacheFirst
    - Extend cache duration from 5 to 30 minutes
    - Remove network timeout delays
    - Enable offline functionality with cached data

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r4r86no860g"
+    "revision": "0.j5ngpofdqao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
